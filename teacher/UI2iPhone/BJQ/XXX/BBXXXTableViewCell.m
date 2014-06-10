9312854ee854091c5d640ea71b2981963d82427c
qing0602 <zq62@vip.qq.com>
2014-06-10 17:59:30 +0800

    change xxx

--- teacher/UI2iPhone/BJQ/XXX/BBXXXTableViewCell.m
+++ teacher/UI2iPhone/BJQ/XXX/BBXXXTableViewCell.m
@@ -122,7 +122,7 @@
     }
     
     NSString *imageUrl = data.imageUrl;
-    if ([imageUrl isEqual:[NSNull null]] || imageUrl.length == 0 || !imageUrl ) {
+    if ([imageUrl isEqual:[NSNull null]] || [imageUrl isEqualToString:@""] ) {
         contentPreView.hidden = NO;
         thumbnail.hidden = YES;
         contentPreView.text = data.content;
