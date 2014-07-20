5fbb088f4b596cea0bb66946b796a5cc7bae1316
qing0602 <zq62@vip.qq.com>
2014-07-20 19:26:03 +0800

    fix bug

--- teacher/UI2iPhone/ZJZ/Contacts/ContactsViewController.m
+++ teacher/UI2iPhone/ZJZ/Contacts/ContactsViewController.m
@@ -54,18 +54,25 @@
         [back addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
         self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:back];
 
-        UISegmentedControl *segement =  [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"",@"", nil]];
-        segement.tintColor = [UIColor clearColor];
-        [segement setWidth:0.1 forSegmentAtIndex:0];
-        [segement setWidth:0.1 forSegmentAtIndex:1];
-        segement.segmentedControlStyle = UISegmentedControlStyleBar;
-        segement.selectedSegmentIndex = 0;
-        [segement addTarget:self action:@selector(segementValueChanged:) forControlEvents:UIControlEventValueChanged];
-        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Teacher"] forLeftSegmentState:UIControlStateSelected rightSegmentState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Teacher"] forLeftSegmentState:UIControlStateHighlighted rightSegmentState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Parent"] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
-        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Parent"] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
-        self.navigationItem.titleView = segement;
+//        UISegmentedControl *segement =  [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"",@"", nil]];
+//        segement.tintColor = [UIColor clearColor];
+//        [segement setWidth:0.1 forSegmentAtIndex:0];
+//        [segement setWidth:0.1 forSegmentAtIndex:1];
+//        segement.segmentedControlStyle = UISegmentedControlStyleBar;
+//        segement.selectedSegmentIndex = 0;
+//        [segement addTarget:self action:@selector(segementValueChanged:) forControlEvents:UIControlEventValueChanged];
+//        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Teacher"] forLeftSegmentState:UIControlStateSelected rightSegmentState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+//        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Teacher"] forLeftSegmentState:UIControlStateHighlighted rightSegmentState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+//        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Parent"] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
+//        [segement setDividerImage:[UIImage imageNamed:@"ZJZ_Seg_Parent"] forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
+//        self.navigationItem.titleView = segement;
+        
+        UIButton *segementBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [segementBtn setFrame:CGRectMake(0.f, 0.f, 128.f, 27.f)];
+        [segementBtn setBackgroundImage:[UIImage imageNamed:@"ZJZ_Seg_Teacher"] forState:UIControlStateNormal];
+        [segementBtn setBackgroundImage:[UIImage imageNamed:@"ZJZ_Seg_Parent"] forState:UIControlStateSelected];
+        [segementBtn addTarget:self action:@selector(segeValueChanged:) forControlEvents:UIControlEventTouchUpInside];
+        self.navigationItem.titleView = segementBtn;
         
         searchResultList = [[NSMutableArray alloc] init];
     }
@@ -197,6 +204,18 @@
     }
 }
 #pragma mark ContactsViewController
+-(void)segeValueChanged :(UIButton *)sender
+{
+    sender.selected = !sender.selected;
+    
+    if (sender.selected) {
+        type = CONTACT_TYPE_PARENT;
+    }else
+    {
+        type = CONTACT_TYPE_TEACHER;
+    }
+    [self.contactsListTableview reloadData];
+}
 -(void)segementValueChanged:(UISegmentedControl *)seg
 {
     if (seg.selectedSegmentIndex == 0) {
