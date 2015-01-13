ef525f32bd42dffcfc7e8b6ab3bebf3948cb89b9
songxin <sxsoul@163.com>
2015-01-13 19:22:28 +0800

    fx bg

--- teacher/UI2iPhone/FX/BBFXViewController.m
+++ teacher/UI2iPhone/FX/BBFXViewController.m
@@ -59,7 +59,7 @@
     [fxTableView setDataSource:(id<UITableViewDataSource>)self];
     
     UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, fxTableView.frame.size.width, fxTableView.frame.size.height)];
-    [backgroundView setBackgroundColor:[UIColor colorWithRed:242.f/255.f green:242.f/255.f blue:242.f/255.f alpha:1.0f]];
+    [backgroundView setBackgroundColor:[UIColor whiteColor]];
     [fxTableView setBackgroundView:backgroundView];
     backgroundView = nil;
     [self.view addSubview:fxTableView];
